a68e897e19a4e828b4893740424f2fa0c8b49a24
Wesley Cabus <Wesley.Cabus@realdolmen.com>
2016-05-13 15:31:01 +0200

    CSRF demo

---
+++ Breaking.AspNet/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+
+[assembly: OwinStartup(typeof(Breaking.AspNet.Startup))]
+
+namespace Breaking.AspNet
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                DefaultFileNames = new []{ "index.html" }
+            });
+
+            app.UseStaticFiles();
+        }
+    }
+}
