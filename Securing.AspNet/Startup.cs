82624b12de83b2d4ceccd81aa226e1703d91eaf8
Wesley Cabus <wesley@gotsharp.be>
2016-05-14 16:38:48 +0200

    Added Web API

--- Securing.AspNet/Startup.cs
+++ Securing.AspNet/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Securing.AspNet.Startup))]
@@ -9,6 +11,24 @@ namespace Securing.AspNet
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            ConfigureWebApi(app);
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            // Disable the XML formatter by removing all media types assigned to it
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            // Prettify the JSON output
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            jsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
         }
     }
 }
